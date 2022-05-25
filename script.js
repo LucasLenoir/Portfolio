a5b703d0c73b5851c6a9ed94a4428097f7353d84
LucasLenoir <95701721+LucasLenoir@users.noreply.github.com>
2022-05-25 14:41:06 +0200

    :adhesive_bandage: Patch ScrollFunction

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use-strict";
 
-
 import "./assets/styles/css/output.css";
 import { MenuController } from "./build/controllers/Menu.controller.js";
 import { CreateMenuSvg } from "./build/svg/menu.svg.js";
@@ -26,48 +25,57 @@ checkIfMobileAndApllyStyle();
 function scroll() {
   if (window.innerWidth > 640) {
     let index = 0;
+    let wheelcount = 0;
     window.addEventListener("wheel", (e) => {
-      if (e.deltaY > 0) {
-        index++;
-      }
-      if (e.deltaY < 0) {
-        index--;
-      }
-      if (index > 3) {
-        index = 0;
-      }
-      if (index < 0) {
-        index = 3;
-      }
+      wheelcount++;
 
-      let fo = document.querySelectorAll("foreignObject");
-      fo.forEach((el) => {
-        el.removeAttribute("class");
-      });
-      fo[index].setAttribute("class", "glow");
-      switch (index) {
-        case 0:
-          HomeView();
-          Container.className = "container_home";
+      if (wheelcount === 6) {
+        if (e.deltaY > 0) {
+          index++;
+        }
+        if (e.deltaY < 0) {
+          index--;
+        }
+        if (index > 3) {
+          index = 0;
+        }
+        if (index < 0) {
+          index = 3;
+        }
 
-          break;
-        case 1:
-          AboutView();
-          Container.className = "container_about";
-          break;
-        case 2:
-          CasesView();
-          Container.className = "container_cases";
+        let fo = document.querySelectorAll("foreignObject");
+        fo.forEach((el) => {
+          el.removeAttribute("class");
+        });
+        fo[index].setAttribute("class", "glow");
+        switch (index) {
+          case 0:
+            HomeView();
+            Container.className = "container_home";
+            wheelcount = 0;
 
-          break;
-        case 3:
-          ContactView();
-          Container.className = "container_contact";
+            break;
+          case 1:
+            AboutView();
+            Container.className = "container_about";
+            wheelcount = 0;
+            break;
+          case 2:
+            CasesView();
+            Container.className = "container_cases";
+            wheelcount = 0;
 
-          break;
+            break;
+          case 3:
+            ContactView();
+            Container.className = "container_contact";
+            wheelcount = 0;
 
-        default:
-          break;
+            break;
+
+          default:
+            break;
+        }
       }
     });
   }
